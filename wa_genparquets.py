0f436f6c04be0963a328d62cbb83509e0b3eaa87
jzphlp <josephluntadilap@gmail.com>
2025-01-18 21:38:12 +0000

    wa_gen_parquuets - tile files to parquets 2

--- wa_genparquets.py
+++ wa_genparquets.py
@@ -1,7 +1,7 @@
 from uvars import tilenames_lidar,RES_DPATH,vending_all,nending_all,ftnames
 from utilsdf import tile_files_to_parquet_parallel
 
-X=90
+X = 90
 tilenames = tilenames_lidar
 
 if __name__ == '__main__':
