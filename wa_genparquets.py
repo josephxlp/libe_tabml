0e0d4135831af0996de9408b4e4f1272eff719c3
jzphlp <josephluntadilap@gmail.com>
2025-01-20 21:52:02 +0000

    Your commit message

--- wa_genparquets.py
+++ wa_genparquets.py
@@ -3,8 +3,23 @@ from uvars import aux_ending30,s1_ending30,s2_ending30,tar_ending30
 from uvars import aux_ending90,s1_ending90,s2_ending90,tar_ending90
 from uvars import aux_ending12,s1_ending12,s2_ending12,tar_ending12
 from utilsdf import tile_files_to_parquet_parallel
+import time 
 
+
+# 90:1, 30:1 12:1
+#X=30 
+#X=12
 X=90
+
+if X == 12:
+    tar_ending,aux_ending,s1_ending,s2_ending = aux_ending12,s1_ending12,s2_ending12,tar_ending12
+
+elif X == 30:
+    tar_ending,aux_ending,s1_ending,s2_ending = aux_ending30,s1_ending30,s2_ending30,tar_ending30
+
+elif X == 90:
+    tar_ending,aux_ending,s1_ending,s2_ending = aux_ending90,s1_ending90,s2_ending90,tar_ending90
+
 tilenames = tilenames_lidar
 vending_all = tar_ending+aux_ending+s1_ending+s2_ending
 
